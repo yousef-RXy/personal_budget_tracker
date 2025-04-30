fcf30b12afca165b20e078bab65efb778dfefa76
Yousef Na <yousefnasr0541@gmail.com>
2025-04-30 21:31:20 +0300

    auth

--- src/main/java/com/se/personal_budget_tracker/Controller/AuthController.java
+++ src/main/java/com/se/personal_budget_tracker/Controller/AuthController.java
@@ -13,13 +13,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
 public class AuthController {
 
   @Autowired
   private UserService userService;
 
-  @PostMapping("/login")
+  @GetMapping("/login")
   public boolean login(UserModel user) {
     return true;
   }
--- src/main/java/com/se/personal_budget_tracker/configuration/SecurityConfig.java
+++ src/main/java/com/se/personal_budget_tracker/configuration/SecurityConfig.java
@@ -12,19 +12,21 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-        .csrf(csrf -> csrf.disable()) // Disable CSRF for simplicity (not recommended for production)
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/register", "/login", "/css/**").permitAll()
-            .anyRequest().authenticated())
-        .formLogin(form -> form
-            .loginPage("/login")
-            .defaultSuccessUrl("/dashboard", true) // Redirect after successful login
-            .permitAll())
-        .logout(logout -> logout
-            .logoutUrl("/logout") // This is the logout URL
-            .logoutSuccessUrl("/login?logout") // Redirect to login page after successful logout
-            .permitAll());
+    // http
+    // .csrf(csrf -> csrf.disable()) // Disable CSRF for simplicity (not recommended
+    // for production)
+    // .authorizeHttpRequests(auth -> auth
+    // .requestMatchers("/register", "/login", "/css/**").permitAll()
+    // .anyRequest().authenticated())
+    // .formLogin(form -> form
+    // .loginPage("/login")
+    // .defaultSuccessUrl("/dashboard", true) // Redirect after successful login
+    // .permitAll())
+    // .logout(logout -> logout
+    // .logoutUrl("/logout") // This is the logout URL
+    // .logoutSuccessUrl("/login?logout") // Redirect to login page after successful
+    // logout
+    // .permitAll());
     return http.build();
   }
 }
